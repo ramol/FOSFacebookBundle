63ae91073c8acdd6b971cef3d18c6ce1d021b1e3
ramol <ramol@ramol.pl>
2013-01-31 13:01:27 +0100

    Update DependencyInjection/Security/Factory/FacebookFactory.php

--- DependencyInjection/Security/Factory/FacebookFactory.php
+++ DependencyInjection/Security/Factory/FacebookFactory.php
@@ -23,6 +23,7 @@ class FacebookFactory extends AbstractFactory
         $this->addOption('display', 'page');
         $this->addOption('app_url');
         $this->addOption('server_url');
+        $this->addOption('redirect_uri_overwrite');
         $this->addOption('create_user_if_not_exists', false);
         $this->addOption('redirect_to_facebook_login', true);
     }
